123af6f4ac65da0c011845c0a993c98daadf2974
Denis Buryi <denis@buryi.de>
2022-04-15 12:55:50 +0700

    md_to_html

---
+++ Makefile
@@ -0,0 +1,2 @@
+start.html:
+	.venv/bin/python md_to_html.py ~/org/Zettelkasten/buryi_de/start.md > start.html
--- md_to_html.py
+++ md_to_html.py
@@ -3,8 +3,36 @@
 import markdown
 import sys
 
-for filename in sys.argv[1:]:
-    with open(filename, 'r') as f:
-        output = markdown.markdown(f.read())
-        print(output)
+
+def one_template(filenames):
+    template = open('template.html').read()
+
+    for filename in filenames:
+        with open(filename, 'r') as f:
+            body = markdown.markdown(f.read())
+            title = filename.rsplit('/', 1)[-1].rsplit('.', 1)[0]
+            with open(f"{title}.html", "w") as out_file:
+                result = template % dict(title=title, body=body)
+                out_file.write(result)
+
+
+def md_and_template(filenames, stdout=True):
+    for filename in filenames:
+        with open(filename, 'r') as f:
+            body = markdown.markdown(f.read())
+        template_name = filename.rsplit('.', 1)[0] + '_template.html'
+        with open(template_name, 'r') as f:
+            template = f.read()
+        title = filename.rsplit('/', 1)[-1].rsplit('.', 1)[0]
+        result = template % dict(title=title, body=body)
+
+        if stdout:
+            print(result)
+        else:
+            with open(f"{title}.html", "w") as out_file:
+                out_file.write(result)
+
+
+if __name__ == "__main__":
+    md_and_template(sys.argv[1:])
 
