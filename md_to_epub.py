6e13db28869f761173a3f25dfc32816700c25eac
Denis Buryi <denis@buryi.ru>
2025-04-29 21:18:44 +0700

    imgs cache

--- md_to_epub.py
+++ md_to_epub.py
@@ -3,10 +3,41 @@
 import markdown
 from pathlib import Path
 import sys
+import shelve
+import hashlib
 
 import requests
 from ebooklib import epub
 from bs4 import BeautifulSoup
+
+CACHE_DIR = './cache'
+CACHE_FILE = './cache.shelve'
+
+
+def save_imgs(filenames):
+    with shelve.open(CACHE_FILE) as cache_db:
+        for i, filename in enumerate(filenames):
+            with open(filename, 'r') as f:
+                body = markdown.markdown(f.read())
+            soup = BeautifulSoup(body, "html.parser")
+            img_tags = soup.find_all('img')
+            for j, img in enumerate(img_tags):
+                url = img['src']
+                if url in cache_db:
+                    continue
+                img_name = str(url).rsplit('/', 1)[-1]
+                response = requests.get(url)
+                media_type = response.headers['Content-Type']
+                hash = str(hashlib.sha1(response.content))
+                with open(Path(CACHE_DIR) / hash, 'wb') as fp:
+                    fp.write(response.content)
+                cache_db[url] = dict(
+                    img_name=img_name,
+                    media_type=media_type,
+                    hash=hash,
+                )
+                print('.', end='')
+    print('all imgs saved')
 
 
 def md_to_epub(filenames, name):
@@ -28,16 +59,16 @@ def md_to_epub(filenames, name):
         soup = BeautifulSoup(body, "html.parser")
         img_tags = soup.find_all('img')
         for j, img in enumerate(img_tags):
-            url = img['src']
-            img_name = str(url).rsplit('/', 1)[-1]
-            response = requests.get(url)
-            media_type = response.headers['Content-Type']
-            img_file_path = f'static/{img_name}'
-            img['src'] = img_file_path
-            eimg = epub.EpubImage(uid=f'image_{i}_{j}', file_name=img_file_path, media_type=media_type, content=response.content)
-            book.add_item(eimg)
-            print('.', end='')
-        print()
+            with shelve.open(CACHE_FILE) as cache_db:
+                if cached_img := cache_db.get(img['src']):
+                    img_name = cached_img['img_name']
+                    media_type = cached_img['media_type']
+                    img_file_path = f'static/{img_name}'
+                    img['src'] = img_file_path
+                    with open(Path(CACHE_DIR) / cached_img['hash'], 'rb') as img_file:
+                        content = img_file.read()
+                    eimg = epub.EpubImage(uid=f'image_{i}_{j}', file_name=img_file_path, media_type=media_type, content=content)
+                    book.add_item(eimg)
 
         chapter = epub.EpubHtml(title=title, file_name=f"chap{i}.xhtml", lang="ru")
         chapter.content = str(soup)
@@ -55,6 +86,7 @@ def md_to_epub(filenames, name):
 
 if __name__ == "__main__":
     filenames = sys.argv[1:]
+    save_imgs(filenames)
     base_name = "2025-04-25"
     max_files = 5
     if len(filenames) > max_files:
